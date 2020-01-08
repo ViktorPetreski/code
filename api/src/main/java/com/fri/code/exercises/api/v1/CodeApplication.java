14414c98239f80b20c4b0f6a8dd244d4447bcd98
ViktorPetreski <vpetreski96@gmail.com>
2020-01-09 00:01:28 +0100

    add logging and openapi

--- api/src/main/java/com/fri/code/exercises/api/v1/CodeApplication.java
+++ api/src/main/java/com/fri/code/exercises/api/v1/CodeApplication.java
@@ -1,12 +1,16 @@
 package com.fri.code.exercises.api.v1;
 
 import com.kumuluz.ee.discovery.annotations.RegisterService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
 @ApplicationPath("/v1")
 @RegisterService
+@OpenAPIDefinition(info = @Info(title = "ExercisesAPI", version = "v1.0.0"), servers = @Server(url = "http://34.67.168.202:8080/v1"), security = @SecurityRequirement(name = "openid-connect"))
 public class CodeApplication extends Application {
 
 }
--- api/src/main/java/com/fri/code/exercises/api/v1/resources/ExerciseMetadataResource.java
+++ api/src/main/java/com/fri/code/exercises/api/v1/resources/ExerciseMetadataResource.java
@@ -5,6 +5,13 @@ import com.fri.code.exercises.api.v1.dtos.ApiError;
 import com.fri.code.exercises.lib.ExerciseMetadata;
 import com.fri.code.exercises.lib.OutputMetadata;
 import com.fri.code.exercises.services.beans.ExerciseMetadataBean;
+import com.kumuluz.ee.logs.cdi.Log;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -17,6 +24,7 @@ import javax.ws.rs.core.UriInfo;
 import java.util.List;
 import java.util.Map;
 
+@Log
 @ApplicationScoped
 @Path("/exercises")
 @Produces(MediaType.APPLICATION_JSON)
@@ -31,6 +39,11 @@ public class ExerciseMetadataResource {
     protected UriInfo uriInfo;
 
     @GET
+    @Operation(summary = "Get all exercises details", description = "Returns all exercises.")
+    @ApiResponses({
+            @ApiResponse(description = "List of all exercises", responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation =
+                    ExerciseMetadata.class))))
+    })
     @Timed
     public Response getExerciseMetadata() {
         List<ExerciseMetadata> exerciseMetadata = exerciseMetadataBean.getExercisesMetadata();
@@ -45,17 +58,29 @@ public class ExerciseMetadataResource {
     }
 
     @GET
+    @Operation(summary = "Get exercise details", description = "Returns details for chosen exercise id")
+    @ApiResponses({
+            @ApiResponse(description = "Exercise details", responseCode = "200", content = @Content(schema = @Schema(implementation =
+                    ExerciseMetadata.class))),
+            @ApiResponse(description = "Exercise not found", responseCode = "404")
+    })
     @Path("/{exerciseID}")
     public Response getExerciseMetadata(@PathParam("exerciseID") Integer exerciseID) {
         try {
             ExerciseMetadata exerciseMetadata = exerciseMetadataBean.getExerciseMetadata(exerciseID);
             return Response.status(Response.Status.OK).entity(exerciseMetadata).build();
-        }catch (Exception e){
+        } catch (Exception e) {
             return Response.status(Response.Status.NOT_FOUND).entity(getNotFoundApiError(e.getMessage())).build();
         }
     }
 
     @POST
+    @Operation(summary = "Add exercise", description = "Adds exercise and returns it")
+    @ApiResponses({
+            @ApiResponse(description = "New exercise", responseCode = "200", content = @Content(schema = @Schema(implementation =
+                    ExerciseMetadata.class))),
+            @ApiResponse(description = "Can not create exercise", responseCode = "400")
+    })
     public Response createExerciseMetadata(ExerciseMetadata exerciseMetadata) {
         if ((exerciseMetadata.getContent() == null) || (exerciseMetadata.getDescription() == null) || (exerciseMetadata.getSubjectID() == null)) {
             ApiError error = new ApiError();
@@ -63,8 +88,7 @@ public class ExerciseMetadataResource {
             error.setMessage("You are missing some of the parameters");
             error.setStatus(Response.Status.BAD_REQUEST.getStatusCode());
             return Response.status(Response.Status.BAD_REQUEST).entity(error).build();
-        }
-        else
+        } else
             exerciseMetadata = exerciseMetadataBean.createExerciseMetadata(exerciseMetadata);
 
         return Response.status(Response.Status.OK).entity(exerciseMetadata).build();
@@ -72,6 +96,12 @@ public class ExerciseMetadataResource {
     }
 
     @PUT
+    @Operation(summary = "Update exercise", description = "Updates chosen exercise and returns it")
+    @ApiResponses({
+            @ApiResponse(description = "Updated exercise", responseCode = "200", content = @Content(schema = @Schema(implementation =
+                    ExerciseMetadata.class))),
+            @ApiResponse(description = "Exercise not found", responseCode = "404")
+    })
     @Path("{exerciseID}")
     public Response updateExerciseMetadata(@PathParam("exerciseID") Integer exerciseID, ExerciseMetadata newExercise) {
         newExercise = exerciseMetadataBean.putExerciseMetadata(exerciseID, newExercise);
@@ -82,17 +112,28 @@ public class ExerciseMetadataResource {
     }
 
     @DELETE
+    @Operation(summary = "Delete exercise", description = "Deletes chosen exercise")
+    @ApiResponses({
+            @ApiResponse(description = "Exercise deleted", responseCode = "204"),
+            @ApiResponse(description = "Exercise not found", responseCode = "404")
+    })
     @Path("/{exerciseID}")
     public Response deleteExerciseMetadata(@PathParam("exerciseID") Integer exerciseID) {
         if (exerciseMetadataBean.deleteExerciseMetadata(exerciseID)) {
             return Response.status(Response.Status.NO_CONTENT).build();
-        }
-        else{
+        } else {
             return Response.status(Response.Status.NOT_FOUND).entity(getNotFoundApiError("")).build();
         }
     }
 
     @GET
+    @Operation(summary = "Get all outputs for exercise", description = "Outputs for specified exercise")
+    @ApiResponses({
+            @ApiResponse(description = "List of outputs", responseCode = "200", content = @Content(schema = @Schema(implementation =
+                    OutputMetadata.class))),
+            @ApiResponse(description = "Exercise not found", responseCode = "404"),
+            @ApiResponse(description = "Outputs service is not available", responseCode = "503")
+    })
     @Path("{exerciseID}/outputs")
     public Response getOutputsForExercise(@PathParam("exerciseID") Integer exerciseID) {
         try {
@@ -112,6 +153,11 @@ public class ExerciseMetadataResource {
     }
 
     @GET
+    @Operation(summary = "Get exercises for specified subject", description = "Returns list of exercises for subject")
+    @ApiResponses({
+            @ApiResponse(description = "Exercise details", responseCode = "200", content = @Content(schema = @Schema(implementation =
+                    ExerciseMetadata.class)))
+    })
     @Path("/subject/{subjectID}")
     public Response getExercisesForSubject(@PathParam("subjectID") Integer subjectID) {
         List<ExerciseMetadata> exercises = exerciseMetadataBean.getExercisesForSubject(subjectID);
@@ -120,7 +166,7 @@ public class ExerciseMetadataResource {
 
     private ApiError getNotFoundApiError(String message) {
         ApiError error = new ApiError();
-        if(message.isEmpty()) message = "The exercise was not found";
+        if (message.isEmpty()) message = "The exercise was not found";
         error.setCode(Response.Status.NOT_FOUND.toString());
         error.setMessage(message);
         error.setStatus(Response.Status.NOT_FOUND.getStatusCode());
