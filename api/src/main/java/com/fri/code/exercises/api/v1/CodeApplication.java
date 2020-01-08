855c296b82fa2fac65143f0c0db9c25bb6f9d888
ViktorPetreski <vpetreski96@gmail.com>
2020-01-09 00:34:56 +0100

    bug fix

--- api/src/main/java/com/fri/code/exercises/api/v1/CodeApplication.java
+++ api/src/main/java/com/fri/code/exercises/api/v1/CodeApplication.java
@@ -2,15 +2,21 @@ package com.fri.code.exercises.api.v1;
 
 import com.kumuluz.ee.discovery.annotations.RegisterService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+@SecurityScheme(name = "openid-connect", type = SecuritySchemeType.OPENIDCONNECT,
+        openIdConnectUrl = "http://auth-server-url/.well-known/openid-configuration")
 @ApplicationPath("/v1")
 @RegisterService
-@OpenAPIDefinition(info = @Info(title = "ExercisesAPI", version = "v1.0.0", contact = @Contact()), servers = @Server(url = "http://34.67.168.202:8080/v1"))
+@OpenAPIDefinition(info = @Info(title = "ExercisesAPI", version = "v1.0.0", contact = @Contact(), license = @License()), security = @SecurityRequirement(name = "openid-connect"), servers = @Server(url = "http://34.67.168.202:8080/v1"))
 public class CodeApplication extends Application {
 
 }
